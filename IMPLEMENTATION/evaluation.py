c99514701b9a27cc09831abdfd1fe260f7a5c8ec
Sharika P K <sharikavipanchika@gmail.com>
2025-07-28 00:18:18 +0530

    Create evaluation.py

---
+++ IMPLEMENTATION/evaluation.py
@@ -0,0 +1,21 @@
+def plot_history(history):
+    plt.figure(figsize=(12, 4))
+    
+    # Plot Accuracy
+    plt.subplot(1, 2, 1)
+    plt.plot(history.history['accuracy'], label='Train Accuracy')
+    plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+    plt.title('Accuracy over epochs')
+    plt.legend()
+    
+    # Plot Loss
+    plt.subplot(1, 2, 2)
+    plt.plot(history.history['loss'], label='Train Loss')
+    plt.plot(history.history['val_loss'], label='Validation Loss')
+    plt.title('Loss over epochs')
+    plt.legend()
+    
+    plt.tight_layout()
+    plt.show()
+
+plot_history(history)
